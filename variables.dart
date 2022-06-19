a1276786af5b360b3bd922841f27e4a7d6123816
Hudaib Alhudaib <hudaib@Hudaibs-MacBook-Pro.local>
2022-06-19 16:32:09 +0300

    task done

---
+++ variables.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+void main() {
+
+String firstName ="Hudaib";
+String lastName ="Al Hudaib";
+int age=29;
+double height=1.76;
+bool married=true;
+  
+double temperature=30;
+String drink="juice";
+String flavor="orange";
+
+print("The temperature is ${temperature}C");
+print("I like $flavor $drink");
+
+
+int number=5;
+
+print(" $number + $number makes 10");
+
+
+String fullName = "John doe";
+
+
+// My name is  JOHN and my last name length is 3
+print(fullName.substring(0,4));
+print(fullName.length.);  //??????????
+
+
+// Does My Last Name Starts With The Letter d?
+// Using the same docs, look for a method that checks if the last name: doe starts with the letter d.
+print(lastName.); 
+
+}
